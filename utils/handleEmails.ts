ce4d4dbf3335a520890081a3848d25adff01f599
Abdulla-Ababakre <abdullaszu@gmail.com>
2024-02-07 15:06:28 -0800

    fix: deploy error remove value check for email handler funciton

--- utils/handleEmails.ts
+++ utils/handleEmails.ts
@@ -1,5 +1,6 @@
 export const handleEmails = (arr:any) => {
     return arr.flatMap((obj:any) =>
+      // @ts-ignore
       Object.values(obj).filter((value) => value !== "" && value.includes("@"))
     );
   };
