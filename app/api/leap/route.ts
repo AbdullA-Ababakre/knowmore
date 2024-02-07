f5d617aa68f6513f6a8929d097322c171c864f1d
Abdulla-Ababakre <abdullaszu@gmail.com>
2024-02-07 04:03:31 -0800

    fix: deploy fix

--- app/api/leap/route.ts
+++ app/api/leap/route.ts
@@ -9,18 +9,36 @@ const leap = new Leap({
   apiKey: "le_6ee3a58a_10BQlbAOmDeD0OaYuFpt76jR",
 });
 
-export async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const response = await leap.workflowRuns.workflow({
-    workflow_id: "wkf_D8rw0tlFr5t0d3",
-    webhook_url: "https://26f9-204-11-230-50.ngrok-free.app/api/leap",
-    input: {
-      email_of_lead: "bill.gates@microsoft.com",
-    },
-  });
-  console.log("response", response);
-  if (response.data.output !== null) {
-    return Response.json(response.data);
-  } else {
-    return Response.json("not completed");
+export async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method === "POST") {
+    const response = await leap.workflowRuns.workflow({
+      workflow_id: "wkf_D8rw0tlFr5t0d3",
+      webhook_url: "https://26f9-204-11-230-50.ngrok-free.app/api/leap",
+      input: {
+        email_of_lead: "bill.gates@microsoft.com",
+      },
+    });
+    console.log("response", response);
+    if (response.data.output !== null) {
+      return Response.json(response.data);
+    } else {
+      return Response.json("not completed");
+    }
   }
+}
+
+export async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
+  // const response = await leap.workflowRuns.workflow({
+  //   workflow_id: "wkf_D8rw0tlFr5t0d3",
+  //   webhook_url: "https://26f9-204-11-230-50.ngrok-free.app/api/leap",
+  //   input: {
+  //     email_of_lead: "bill.gates@microsoft.com",
+  //   },
+  // });
+  // console.log("response", response);
+  // if (response.data.output !== null) {
+  //   return Response.json(response.data);
+  // } else {
+  //   return Response.json("not completed");
+  // }
 }
