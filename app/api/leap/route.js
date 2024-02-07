45f69c63252fed54e333477874e1738549fb5ea9
Abdulla-Ababakre <abdullaszu@gmail.com>
2024-02-07 14:57:56 -0800

    fix: deploy fix

--- app/api/leap/route.js
+++ app/api/leap/route.js
@@ -4,31 +4,26 @@ const leap = new Leap({
   apiKey: "le_80593dca_E05YaZ9T0h69tXw0iHDkbCHm",
 });
 
-
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const response = await leap.workflowRuns.workflow({
-        workflow_id: "wkf_ZUV578fRsUfxz3",
-        webhook_url: "https://26f9-204-11-230-50.ngrok-free.app/api/leap",
-        input: {
-          email_of_lead: "bill.gates@microsoft.com",
-        },
-      });
+// export default async function handler(req, res)
+export async function POST(req, res) {
+  try {
+    const response = await leap.workflowRuns.workflow({
+      workflow_id: "wkf_ZUV578fRsUfxz3",
+      webhook_url: "https://26f9-204-11-230-50.ngrok-free.app/api/leap",
+      input: {
+        email_of_lead: "bill.gates@microsoft.com",
+      },
+    });
 
-      if (response.data.output !== null) {
-        res.json(response.data.output); // Assuming output is of type Data
-      } else {
-        res.json({ name: "not completed" });
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ name: "Internal server error" });
+    if (response.data.output !== null) {
+      res.json(response.data.output); // Assuming output is of type Data
+    } else {
+      res.json({ name: "not completed" });
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ name: "Internal server error" });
   }
 }
 
--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -8,54 +8,51 @@ const stripe = new Stripe(process.env.STRIPE_SK_KEY, {
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const signature = headers().get("stripe-signature");
-    const signingSecret = process.env.STRIPE_SIGNING_SECRET;
-    const rawBody = await buffer(req.body);
+// export default async function handler(req, res) 
+export async function POST(req,res){
+  const signature = headers().get("stripe-signature");
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+  const rawBody = await buffer(req.body);
 
-    let event;
-    try {
-      event = stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
-    } catch (err) {
-      return Response.json({ error: `Webhook Error ${err?.message} ` });
-    }
-
-    switch (event.type) {
-      case "customer.subscription.deleted":
-        const deleteSubscription = event.data.object;
-        if (deleteSubscription.status === "canceled") {
-          await onCacnelSubscription(false, deleteSubscription.customer);
-        }
-        break;
-      case "customer.subscription.updated":
-        const { customer } = event.data.object;
-        const subscription = await stripe.subscriptions.list({
-          customer: customer.id,
-        });
-
-        const customerInfo = await stripe.customers.retrieve(
-          event.data.object.customer
-        );
-
-        if (subscription.data.length && event.data.object.status === "active") {
-          const sub = subscription.data[0];
-          await onSuccessSubscription(
-            sub.id,
-            customer,
-            sub.status === "active",
-            customerInfo.email
-          );
-        }
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    return Response.json({ success: true });
-  } else {
-    return Response.json({ success: false });
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
+  } catch (err) {
+    return Response.json({ error: `Webhook Error ${err?.message} ` });
   }
+
+  switch (event.type) {
+    case "customer.subscription.deleted":
+      const deleteSubscription = event.data.object;
+      if (deleteSubscription.status === "canceled") {
+        await onCacnelSubscription(false, deleteSubscription.customer);
+      }
+      break;
+    case "customer.subscription.updated":
+      const { customer } = event.data.object;
+      const subscription = await stripe.subscriptions.list({
+        customer: customer.id,
+      });
+
+      const customerInfo = await stripe.customers.retrieve(
+        event.data.object.customer
+      );
+
+      if (subscription.data.length && event.data.object.status === "active") {
+        const sub = subscription.data[0];
+        await onSuccessSubscription(
+          sub.id,
+          customer,
+          sub.status === "active",
+          customerInfo.email
+        );
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return Response.json({ success: true });
 }
 
 // export async function POST(req, res) {
