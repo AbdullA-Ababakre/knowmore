0498e993ff2e736cacba2d94c1cde3fd83e7e58a
Abdulla-Ababakre <abdullaszu@gmail.com>
2024-02-07 18:14:19 -0800

    feat: leapcomplete

--- app/api/leapcomplete/route.js
+++ app/api/leapcomplete/route.js
@@ -1,4 +1,14 @@
-export async function GET(req, res) {
-  console.log("leapcomplete", res.body);
-  return Response.json({ message: "leapcomplete" });
+// export async function GET(req, res) {
+//   console.log("leapcomplete", res.body);
+//   return Response.json({ message: "leapcomplete" });
+// }
+
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const incomingData = await request.json();
+  // console.log(incomingData, "train model webhook incomingData");
+
+  const { output } = incomingData;
+  console.log("output111", output);
 }
